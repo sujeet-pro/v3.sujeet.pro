2a547d576b56598085b823c698c264d5a1e1ad0e
Sujeet Jaiswal <sujeet.profession@gmail.com>
2025-02-06 03:36:50 +0530

    chore: added search functionality

--- astro.config.ts
+++ astro.config.ts
@@ -2,15 +2,26 @@ import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import { transformerCopyButton } from '@rehype-pretty/transformers'
 import tailwindcss from '@tailwindcss/vite'
+import pagefind from 'astro-pagefind'
 import { defineConfig } from 'astro/config'
 import rehypePrettyCode from 'rehype-pretty-code'
+
 // TODO: path alias doesn't work on astro.config.ts
 import { SITE_CANONICAL_ORIGIN } from './src/configs/site.constants'
 
 // https://astro.build/config
 export default defineConfig({
   site: SITE_CANONICAL_ORIGIN,
-  integrations: [mdx(), sitemap()],
+  base: '/',
+  integrations: [
+    mdx(),
+    sitemap(),
+    pagefind({
+      indexConfig: {
+        verbose: true
+      }
+    })
+  ],
   vite: {
     plugins: [tailwindcss()]
   },
